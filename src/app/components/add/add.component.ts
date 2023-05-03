c7285a73d28d46db41942b7e90678ee56865e409
nosipho legodi <nosipholegodi08@gmail.com>
2023-05-03 10:24:36 +0200

    The add function modifications have bben updated

--- src/app/components/add/add.component.ts
+++ src/app/components/add/add.component.ts
@@ -1,13 +1,61 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ServiceService } from 'src/app/services/service.service';
+
+
 
 @Component({
   selector: 'app-add',
   templateUrl: './add.component.html',
   styleUrls: ['./add.component.css']
 })
-export class AddComponent{}
+export class AddComponent{
+onSubmit() {
+throw new Error('Method not implemented.');
+}
+  empForm : FormGroup;
+
+
+
+  constructor(private _fb :FormBuilder, private _empService: ServiceService, private http: HttpClient ){
+    this.empForm = this._fb.group({
+      EmployeeID : "",
+      Name :"",
+      Surname:"",
+      Email:"",
+      Contact: "",
+      Department:"",
+    });
+  }
+
+  ngOnInit() {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    
+  }
+  onFormSubmit(){
+      if(this.empForm.valid){
+        this._empService.addEmployee(this.empForm.value).subscribe({
+          next: (val: any) => {
+            alert('Employee added successfully');
+          },
+          error: (err: any) => {
+            console.error (err);
+          },
+          
+        });
+    }
+  
+
+
+
+  }
 
 
 
 
 
+
+
+}
