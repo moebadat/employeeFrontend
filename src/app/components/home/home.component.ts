630210c8269a286489e9b04eeab1a6ef7ddc5bb1
moebadat <moebadat16@gmail.com>
2023-05-06 19:22:10 +0200

    comments to home comeponent

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,25 +10,31 @@ import Swal from 'sweetalert2';
 })
 export class HomeComponent {
 
-  deleteConfirmation(Id:any){
+
+  // Sweet alert for the delete button
+  deleteConfirmation(Id: any) {
     Swal.fire({
       title: "Are you sure you want to delete?",
-      text: 'You will not be able to recover this file!',
+      text: 'This recod cannot be recovered!',
       icon: 'warning',
       showCancelButton: true,
       confirmButtonText: 'Yes, delete it!',
       cancelButtonText: 'No, keep it',
     }).then((result) => {
+      // if the obtained value is true(User indicated that they would sure they want to delete), then invole the delete http metjod
       if (result.value) {
-        this.service.deleteEmployee(Id).subscribe( data => {
-          this.ngOnInit(); 
-          this.service.getEmployeeAll();
+        this.service.deleteEmployee(Id).subscribe(data => {
         })
+        // delete confirmation
         Swal.fire(
           'Deleted!',
           'Your imaginary file has been deleted.',
           'success'
         )
+        // reload the page
+        this.ngOnInit();
+
+        // if the user has cabcelled the delete the confirm the deletion
       } else if (result.dismiss === Swal.DismissReason.cancel) {
         Swal.fire(
           'Cancelled',
@@ -37,38 +43,41 @@ export class HomeComponent {
         )
       }
     }
-  )}
+    )
+  }
 
-  constructor(private service:ServiceService, private router: Router) {}
+  constructor(private service: ServiceService, private router: Router) { }
 
-  items:any;
-  // id?: number;
+  items: any;
 
+
+  //when this component is loaded then invoke the get all employees function
   ngOnInit() {
-
-    
-    
     this.service.getEmployeeAll()
+      //open a channel of communication with observable (getAllEmployee()), then assign the obtained data to items
       .subscribe(data => {
         this.items = data;
       });
-}
+  }
 
+  //delete a secific employee
+  delete(Id: number) {
+    this.service.deleteEmployee(Id).subscribe(data => {
+      //once function is done then reload page
+      this.ngOnInit();
+    })
+  }
 
- delete(Id:number){
-  this.service.deleteEmployee(Id).subscribe( data => {
-    this.ngOnInit(); 
-    this.service.getEmployeeAll();
-  })
-}
+  // set id obtained from form to local Storage(persistant storage provided by the browser )
+  setId(Id: any) {
+    localStorage.setItem("id", Id)
+  }
 
-setId(Id:any){
-  localStorage.setItem("id",Id)
-}
+  // method that navigates  to router component and sends the records id that is accessed in ts  through (params["id"])
   viewEmployee(id: number) {
     console.log("Id on home", id);
     this.router.navigate(['/view/' + id])
-    
+
   }
 }
 
